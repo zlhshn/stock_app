d84ecfad8e9909c5032844e896abc94dad4cc010
zlhshn <zsahin4401@gmail.com>
2024-01-08 01:43:25 +0300

    create folder structure

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import PrivateRouter from "./PrivateRouter";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="dashboard" element={<PrivateRouter />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
