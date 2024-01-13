5a6e2acc6515746a6f532f44c9f0382a5a6dab37
zlhshn <zsahin4401@gmail.com>
2024-01-13 11:28:24 +0300

    login-style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import stockImage from "../assets/loginimage.png"
+import stockImage from "../assets/loginimage.png";
 
 const Login = () => {
   return (
-    <div className='bg-[#c3e2c2]  h-[100vh]'>
-      <div>
-        <img src={stockImage} alt=""  className='w-[100px]'/>
-      </div>
-      <div className='w-[50%]'>
-      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+    <div className="bg-[#3a7eeb]  h-[100vh] flex flex-col content-center items-center   ">
+        <div className="basis-full text-black text-center">
+          <h2 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900" >Stock Management</h2>
+        </div>
+      <div className="box-border rounded-lg bg-white bg-opacity-20 shadow-lg backdrop-filter backdrop-blur-md border border-white border-opacity-30 flex flex-wrap justify-around items-center w-[70%] h-[60%]  content-baseline">
+
+      
+        <div className="w-1/2  hidden md:block">
+          <img src={stockImage} alt="" className="w-[100%] h-[100%]"/>
+        </div>
+        <div className="w-1/2   rounded-lg ">
+          <h2 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
-      <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Email address
               </label>
               <div className="mt-2">
@@ -31,11 +40,17 @@ const Login = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
                     Forgot password?
                   </a>
                 </div>
@@ -61,9 +76,10 @@ const Login = () => {
               </button>
             </div>
           </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
